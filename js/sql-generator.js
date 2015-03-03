30e3465762619cf088bc308bc61a851495271c0e
vivalg <vivalg@sina.com>
2015-03-02 21:40:55 -0800

    re-implement

--- js/sql-generator.js
+++ js/sql-generator.js
@@ -1,17 +1,21 @@
 $(function(){
     $('#in1').next('button').click(function(){
+        var mode = $('select:first').val();
+        var c = $('select:first>option:selected').attr('data-params');
+
         var text = $(this).prev().val();
 	var items = text.split(/[\t ,]+/);
-	if(items && items.length == 2){
-	    $(this).nextAll('input').first().val('select * from hotel_linkage where wrapper_id = \'' + items[0] + '\' and site_param = ' + items[1] + ';').focus().select();
+	if(items && items.length == c){
+	    var t = '';
+            if(mode == 0){
+	        t = 'select * from hotel' + '_' + 'linkage where id = ' + items[0] + ';';
+            }else if(mode == 1){
+	        t = 'select * from hotel' + '_' + 'linkage where wrapper_id = \'' + items[0] + '\' and site_param = ' + items[1] + ';';
+	    }else if(mode == 2){
+	        t = 'select * from linkage' + '_' + 'op_log where linkage_id = ' + items[0] + ' order by id desc limit 5;';
+	    }
+	    $(this).nextAll('input').first().val(t).focus().select();
 	}
     });
-    $('#in2').next('button').click(function(){
-        var text = $(this).prev().val();
-	if(text){
-	    $(this).nextAll('input').first().val('select * from hotel_linkage_log where linkage_id = ' + text + ' order by id desc limit 5;').focus().select();
-	}
-    });
-
-    $('input:first').focus();
+    $('select:first').focus();
 });
